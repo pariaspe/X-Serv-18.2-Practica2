0fa7a4f2c498dee2bfead78b9bc91b3be34906b8
Pedro Arias Perez <parias@gamma07.aulas.gsyc.urjc.es>
2018-04-06 10:15:53 +0200

    Creado el modelo y hecha la migracion

--- myproject/shorterapp/admin.py
+++ myproject/shorterapp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from shorterapp.models import URLs
 
 # Register your models here.
+admin.site.register(URLs)
--- myproject/shorterapp/models.py
+++ myproject/shorterapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class URLs(models.Model):
+    url = models.URLField()
+    def __str__(self):
+        return self.url
