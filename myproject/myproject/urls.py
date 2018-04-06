25b09a40d0688a35ec5ea0691ef9cb37e4a53373
Pedro Arias Perez <parias@gamma07.aulas.gsyc.urjc.es>
2018-04-06 09:43:49 +0200

    Primera version que solo implementa un view que devuelve hola para el recurso barra

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -16,5 +16,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^$', 'shorterapp.views.barra'),
     url(r'^admin/', include(admin.site.urls)),
 ]
