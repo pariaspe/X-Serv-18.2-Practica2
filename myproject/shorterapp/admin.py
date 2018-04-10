56b28787b1367bd9dc6405a340820904ac18dae9
Pedro Arias Perez <parias@alpha15.aulas.gsyc.urjc.es>
2018-04-10 17:18:04 +0200

    removing old project

--- myproject/shorterapp/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from shorterapp.models import URLs
-
-# Register your models here.
-admin.site.register(URLs)
--- myproject/shorterapp/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class URLs(models.Model):
-    url = models.URLField(unique=True) # initial='http://', 
-    def __str__(self):
-        return self.url
