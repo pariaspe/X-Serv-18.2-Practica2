9790cb5cff4a2a1d80b6d22d87324658839d83d0
Pedro Arias Perez <parias@alpha15.aulas.gsyc.urjc.es>
2018-04-10 17:52:00 +0200

    Cambiados lolos views para que devuelvan un html correcto

--- project/acorta/views.py
+++ project/acorta/views.py
@@ -8,8 +8,6 @@ from django.views.decorators.csrf import csrf_exempt
 
 from .models import URLs
 
-# TODO: href a la lista; despues de añadir volver a barra; poner todo bonito para html
-
 FORM = """
     <form action="" method="POST">
         <label for="url">URL a acrotar:</label><br>
@@ -34,17 +32,19 @@ def barra(request):
             url.save()
             msg = 'URL añadida!'
         except IntegrityError:
-            msg = 'La URL ya ha sido añadida.'
+            url = URLs.objects.get(url=url)
+            msg = 'La URL ya ha sido añadida en: <a href=/' + str(url.id) + '>/' + str(url.id)  +'</a>'
     elif request.method == 'GET':
         pass
     else:
-        return HttpResponse('Metodo invalido:' + request.method)
+        return HttpResponse('<html><body>Metodo invalido:' + request.method + '</body></html>')
 
     lista = URLs.objects.all()
-    respuesta = '<h1>Bienvenido al acortador de URLs:</h1>' + FORM + msg
-    respuesta += '<h3>Lista de URLs acortadas:</h3>'
+    respuesta = '<html><body><h1>Bienvenido al acortador de URLs:</h1>' + FORM
+    respuesta += msg + '<h3>Lista de URLs acortadas:</h3>'
     for url in lista:
         respuesta += str(url.id) + ' ' + url.url + '<br>'
+    respuesta += '</body></html>'
     return HttpResponse(respuesta)
 
 def numero(request, numero):
@@ -52,4 +52,4 @@ def numero(request, numero):
         url = URLs.objects.get(id=str(numero))
         return HttpResponseRedirect(url)
     except URLs.DoesNotExist:
-        return HttpResponse('No existe pagina')
+        return HttpResponse('<html><body>No existe este recurso acortado.</body></html>')
